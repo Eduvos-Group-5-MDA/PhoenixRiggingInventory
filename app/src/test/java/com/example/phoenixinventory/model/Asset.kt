06df530f32e94837cfa13f95199c6a00fcb42112
Jurgie <66727441+Jurgio@users.noreply.github.com>
2025-10-10 10:51:51 +0200

    Delete app/src/test/java/com/example/phoenixinventory/model directory
    Undoing changes part 2

--- app/src/test/java/com/example/phoenixinventory/model/Asset.kt
+++
@@ -1,9 +0,0 @@
-package com.example.myapp.model
-
-data class Asset(
-    val assetId: Int = 0,
-    val name: String,
-    val status: String,
-    val location: String? = null,
-    val category: String? = null
-)
--- app/src/test/java/com/example/phoenixinventory/model/User.kt
+++
@@ -1,8 +0,0 @@
-package com.example.myapp.model
-
-data class User(
-    val userId: Int = 0,
-    val name: String,
-    val email: String,
-    val role: String? = null
-)
